9ae21dcae73251398cab818613f861839168845f
SakirSajjad <sakir_sajjad@yahoo.com>
2015-05-12 01:14:38 -0700

    java swing
    java swing

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+package javaapplication17;
+
+class Application extends JFrame  {
+    JPanel panel=new JPanel();
+    JLabel label=new JLabel("Name:        ");
+    JLabel label=new JLabel("Password:           ");
+
+    Application(){
+        panel.setLayout(null);
+        add(panel);
+        setSize(700,500);
+        setVisible(true);
+        Elements();
+    }
+    public static void main(String[] args) {
+ 
+    }
+
+}
